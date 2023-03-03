610d6ad60a47a51877128674802c3b48508cc033
nthn5 <99454082+nthn5@users.noreply.github.com>
2023-03-02 23:11:59 -0500

    Update BST.js

--- BST.js
+++ BST.js
@@ -3,16 +3,28 @@ const nodes = (value) => {
 }
 
 const tree = () => {
+    let root;
     const buildTree = (array) => {
-        array.sort((a, b) => {return a - b});
         array = array.filter((item, index) => array.indexOf(item) === index);
-        let root = array[parseInt(array.length / 2)];
-        let node = nodes(root);
+        for (let i in array){
+            array[i] = nodes(array[i]);
+        }
+        root = array[0];
+        let lPointer = root;
+        let rPointer = root;
+        for (let current of array){
+            if (current.value < root.value && current.value < lPointer.value){
+                lPointer.left = current;
+                lPointer = current;
+            }
+        }
         
     }
-    return{buildTree}
+    return{buildTree, get root(){return root}}
 }
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8];
+let array = [10, 2, 9, 1, 4, 5, 6, 7, 8];
 let t = tree();
 t.buildTree(array);
+console.log(t.root.left);
+//left works just need to copy and make it right sided too.
