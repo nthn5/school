98e9cc998cfe37a3ed0bcf05f2defbf92af3fac4
nthn5 <99454082+nthn5@users.noreply.github.com>
2023-03-04 23:05:05 -0500

    buildTree actually works but insert still doesn't

--- BST.js
+++ BST.js
@@ -6,29 +6,50 @@ const tree = () => {
     let root;
     const buildTree = (array) => {
         array = array.filter((item, index) => array.indexOf(item) === index);
-        for (let i in array){
-            array[i] = nodes(array[i]);
-        }
-        root = array[0];
-        let lPointer = root;
-        let rPointer = root;
-        for (let current of array){
-            if (current.value < root.value && current.value < lPointer.value){
-                lPointer.left = current;
-                lPointer = current;
+        root = nodes(array[0]);
+        console.log(array);
+        for (let value of array){
+            if (value < root.value){
+                let current = root;
+                while (current.left !== null){
+                    current = current.left;
+                    if (value > current.value){
+                        while (current.right !== null){
+                            current = current.right;
+                        }
+                        break;
+                    }
+                        
+                }
+                if (value < current.value){
+                    value = nodes(value);
+                    current.left = value;
+                }
+                else if (value > current.value){
+                    value = nodes(value);
+                    current.right = value;
+                }
             }
-            else if (current.value < root.value && current.value > lPointer.value){
-                lPointer.right = current;
-                lPointer = current;
-            }
-
-            else if (current.value > root.value && current.value > rPointer.value){
-                rPointer.right = current;
-                rPointer = current;
-            }
-            else if (current.value > root.value && current.value < rPointer.value){
-                rPointer.left = current;
-                rPointer = current;
+            if (value > root.value){
+                let current = root;
+                while (current.right !== null){
+                    current = current.right;
+                    if (value < current.value){
+                        while (current.left !== null){
+                            current = current.left;
+                        }
+                        break;
+                    }
+                        
+                }
+                if (value > current.value){
+                    value = nodes(value);
+                    current.right = value;
+                }
+                else if (value < current.value){
+                    value = nodes(value);
+                    current.left = value;
+                }
             }
         }
     }
@@ -45,16 +66,34 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     }
 }
 
-let array = [7, 9, 4, 6, 2, 3, 1, 10, 5, 8];
-let t = tree();
-t.buildTree(array);
-console.log(t.root);
-prettyPrint(t.root);
-
-const insert = (value) => {
-    
+const insertAttempt = (value) => {
+    if (value < t.root.value){
+        let current = t.root;
+        while (current.left !== null && value < current.left.value){
+            current = current.left;
+            while (current.right !== null && value > current.right.value){
+                current = current.right;
+            }
+        }
+        if (value < current.value){
+            value = nodes(value);
+            current.left = value;
+        }
+        else if (value > current.value){
+            value = nodes(value);
+            current.right = value;
+        }
+    }
 }
 
 const remove = () => {
 
 }
+
+let array = [7, 9, 14, 8, 5, 4, 6, 3, 1, 2, 12, 13, 11, 10];
+let t = tree();
+t.buildTree(array);
+console.log(t.root);
+prettyPrint(t.root);
+// insertAttempt(5);
+// prettyPrint(t.root);
