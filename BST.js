e84704affbc931ed55fc9010143abb37600f40b8
nthn5 <99454082+nthn5@users.noreply.github.com>
2023-03-06 13:53:46 -0500

    fixed stuff add to actual file

--- BST.js
+++ BST.js
@@ -1,61 +1,3 @@
-const nodes = (value) => {
-    return{value: value, left: null, right: null}
-}
-
-const tree = () => {
-    let root;
-    const buildTree = (array) => {
-        array = array.filter((item, index) => array.indexOf(item) === index);
-        root = nodes(array[0]);
-        console.log(array);
-        for (let value of array){
-            if (value < root.value){
-                let current = root;
-                while (current.left !== null){
-                    current = current.left;
-                    if (value > current.value){
-                        while (current.right !== null){
-                            current = current.right;
-                        }
-                        break;
-                    }
-                        
-                }
-                if (value < current.value){
-                    value = nodes(value);
-                    current.left = value;
-                }
-                else if (value > current.value){
-                    value = nodes(value);
-                    current.right = value;
-                }
-            }
-            if (value > root.value){
-                let current = root;
-                while (current.right !== null){
-                    current = current.right;
-                    if (value < current.value){
-                        while (current.left !== null){
-                            current = current.left;
-                        }
-                        break;
-                    }
-                        
-                }
-                if (value > current.value){
-                    value = nodes(value);
-                    current.right = value;
-                }
-                else if (value < current.value){
-                    value = nodes(value);
-                    current.left = value;
-                }
-            }
-        }
-    }
-    return{buildTree, get root(){return root}}
-}
-
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node.right !== null) {
         prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
@@ -66,34 +8,51 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     }
 }
 
-const insertAttempt = (value) => {
-    if (value < t.root.value){
-        let current = t.root;
-        while (current.left !== null && value < current.left.value){
-            current = current.left;
-            while (current.right !== null && value > current.right.value){
+const nodes = (value) => {
+    return{value: value, left: null, right: null}
+}
+
+const tree = () => {
+    let root;
+
+    const build = (array) => {
+        array = array.filter((item, index) => array.indexOf(item) === index);
+        root = nodes(array[0]);
+        for (value of array){
+            if (value !== root.value){
+                insert(value)
+            }
+        }
+    }
+
+    const insert = (value) => {
+        let current = root;
+        while (true){
+            if (value < current.value){
+                if (!current.left){
+                    current.left = nodes(value);
+                    break;
+                }
+                current = current.left;
+            }
+            if (value > current.value){
+                if (!current.right){
+                    current.right = nodes(value);
+                    break;
+                } 
                 current = current.right;
             }
         }
-        if (value < current.value){
-            value = nodes(value);
-            current.left = value;
-        }
-        else if (value > current.value){
-            value = nodes(value);
-            current.right = value;
-        }
     }
+    return {build, insert, get root(){return root}}
 }
 
-const remove = () => {
+const remove = (value) => {
 
 }
 
-let array = [7, 9, 14, 8, 5, 4, 6, 3, 1, 2, 12, 13, 11, 10];
+let array = [41, 79, 18, 95, 28, 77, 63, 36, 6, 75, 84, 21, 98, 54, 25, 9, 40, 29, 44, 85, 1, 88, 52, 83, 43, 100, 97, 76];
 let t = tree();
-t.buildTree(array);
-console.log(t.root);
+t.build(array);
+t.insert(10);
 prettyPrint(t.root);
-// insertAttempt(5);
-// prettyPrint(t.root);
