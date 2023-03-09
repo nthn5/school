6bb23d7adde42da573e6f42dde2dc4479ced890b
nthn5 <99454082+nthn5@users.noreply.github.com>
2023-03-09 09:38:41 -0500

    Update BST.js

--- BST.js
+++ BST.js
@@ -13,7 +13,7 @@ const nodes = (value) => {
 }
 
 const tree = () => {
-    let root;
+    let root = null;
 
     const build = (array) => {
         array = array.filter((item, index) => array.indexOf(item) === index);
@@ -27,6 +27,10 @@ const tree = () => {
 
     const insert = (value) => {
         let current = root;
+        if (!root){
+            root = nodes(value);
+            return;
+        }
         while (true){
             if (value < current.value){
                 if (!current.left){
@@ -35,7 +39,7 @@ const tree = () => {
                 }
                 current = current.left;
             }
-            if (value > current.value){
+            else if (value > current.value){
                 if (!current.right){
                     current.right = nodes(value);
                     break;
@@ -44,15 +48,133 @@ const tree = () => {
             }
         }
     }
-    return {build, insert, get root(){return root}}
+
+    const remove = (value) => {
+        let current = root;
+        let parent = null;
+        while (current){
+            if (value < current.value){
+                parent = current;
+                current = current.left;
+            }
+            else if (value > current.value){
+                parent = current;
+                current = current.right;
+            }
+            else if (value == current.value){
+                if (current.right == null){
+                    if (parent == null){
+                        root = current.left;
+                    }
+                    else {
+                        if (current.value < parent.value){
+                            parent.left = current.left;
+                        }
+                        else if (current.value > parent.value){
+                            parent.right = current.left;
+                        }
+                    }
+                }
+                else if (current.right.left == null){
+                    current.right.left = current.left;
+                    if (parent == null){
+                        root = current.right;
+                    }
+                    else {
+                        if (current.value < parent.value){
+                            parent.left = current.right;
+                        }
+                        else if (current.value > parent.value){
+                            parent.right = current.right;
+                        }
+                    }
+                }
+                else {
+                    let lm = current.right.left;
+                    let lmparent = current.right;
+                    while (lm.left !== null){
+                        lmparent = lm;
+                        lm = lm.left;
+                    } 
+                    lmparent.left = lm.right;
+                    lm.left = current.left;
+                    lm.right = current.right;
+                    if (parent == null){
+                        root = lm;
+                    }
+                    else {
+                        if (current.value < parent.value){
+                            parent.left = lm;
+                        }
+                        else if (current.value > parent.value){
+                            parent.right = lm;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
+
+    const find = (value) => {
+        let current = root;
+        if (value == root.value){
+            console.log(root);
+            return root;
+        }
+        while (true){
+            if (value < current.value){
+                if (current.left){
+                    if (value == current.left.value){
+                        console.log(current.left);
+                        return current.left;
+                    }
+                    current = current.left;
+                }
+            }
+            else if (value > current.value){
+                if (current.right){
+                    if (value == current.right.value){
+                        console.log(current.right);
+                        return current.right;
+                    }
+                    current = current.right;
+                }
+            }
+            if (current.left == null && current.right == null && value !== current.value){
+                console.log(`${value} not found`);
+                return false;
+            }
+        }
+    }
+
+    const breadth = () => {
+        let array = [];
+        let queue = [root];
+        while (queue[0]){
+            let current = queue[0];
+            if (current.left){
+                queue.push(current.left);
+            }
+            if (current.right){
+                queue.push(current.right);
+            }
+            array.push(current);
+            queue.shift();
+        }
+        console.log(array);
+    }
+
+    return {build, insert, remove, find, breadth, get root(){return root}}
 }
 
-const remove = (value) => {
-
-}
 
 let array = [41, 79, 18, 95, 28, 77, 63, 36, 6, 75, 84, 21, 98, 54, 25, 9, 40, 29, 44, 85, 1, 88, 52, 83, 43, 100, 97, 76];
 let t = tree();
 t.build(array);
 t.insert(10);
 prettyPrint(t.root);
+t.remove(63);
+prettyPrint(t.root);
+t.find(100);
+t.breadth();
