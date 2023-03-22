3dc2c874c84ede714df9ff2091a9dbf43c26fca6
nthn5 <99454082+nthn5@users.noreply.github.com>
2023-03-22 13:45:53 -0400

    Update knights.js

--- knights.js
+++ knights.js
@@ -1,25 +1,23 @@
+//create nodes for linked list
 const nodes = (value) => {
-    return {value: value, next: new Map()}
+    return {value: value, prev: null}
 }
 
-const vertices = (value, move) => {
-    value.next.set(move, [])
-}
-
-const addVertex = (value, vertex) => {
-    
-}
-
-const board = () => {
+//create board
+const drawBoard = () => {
     let board = [];
     for (let i = 1; i <= 8; i++){
         for (let j = 1; j <= 8; j++){
             board.push([i, j]);
         }
     }
-    
-    const legalMoves = (start) => {
-        let legal = board.filter(move => 
+    return board;
+}
+
+//find legal moves
+const legalMoves = (start, board = drawBoard()) => {
+    for (let i = 0; i < board.length; i++){
+        legal = board.filter(move => 
             (move[0] == start[0] + 1 && move[1] == start[1] + 2) ||
             (move[0] == start[0] + 1 && move[1] == start[1] - 2) ||
             (move[0] == start[0] - 1 && move[1] == start[1] + 2) ||
@@ -29,30 +27,48 @@ const board = () => {
             (move[0] == start[0] - 2 && move[1] == start[1] + 1) ||
             (move[0] == start[0] - 2 && move[1] == start[1] - 1)
         );
-        return legal;
     }
-    return {legalMoves}
+    return legal;
 }
-let b = board();
 
+//finds the shortest path and amount of moves taken
 const knightMoves = (start, end) => {
-    let count = 0;
-    let legal = b.legalMoves(start);
-    let root = nodes(start);
-    let current = root;
-    console.log(root)
-    while (true){
-        for (let move in legal){
-            vertices(current, legal[move]);
-            console.log(current.next[move])
-            
-            // legal = b.legalMoves(move);
-            count++;
-            if(count==5){return root;}
-        }
-        current = current.next[move];
+    //checks for valid inputs
+    let inbounds = true;
+    if (start[0] < 1 || start[0] > 8 || start[1] < 1 || start[1] > 8){
+        console.log('start values must be between 1-8');
+        inbounds = false;
     }
-    return root;
+    if (end[0] < 1 || end[0] > 8 || end[1] < 1 || end[1] > 8){
+        console.log('end values must be between 1-8');
+        inbounds = false;
+    }
+    if (inbounds == false){return}
+    //searches for shortest path
+    let path =[];
+    let queue = [nodes(start)];
+    while (queue[0]){
+        let current = queue[0];
+        let legal = legalMoves(queue[0].value);
+        for (let move of legal){
+            move = nodes(move);
+            move.prev = current;
+            if (move.value[0] == end[0] && move.value[1] == end[1]){
+                path.push(move.value);
+                while (move.prev){
+                    path.unshift(move.prev.value);
+                    move = move.prev;
+                }
+                console.log('steps taken:', path.length - 1);
+                console.log('path:', path);
+                return;
+            }
+            queue.push(move);
+        }
+        queue.shift();
+    }
 }
 
-console.log(knightMoves([1, 1], [2, 3]));
+//test cases
+knightMoves([1, 1], [8, 8]); //valid input
+knightMoves([-1, 11], ['11', -1]); //invalid input
